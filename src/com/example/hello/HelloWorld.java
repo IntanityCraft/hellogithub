7524baebd2c18f4a4676652b6a53f6c3586c4dbf
ArchSanta <santazaaa@gmail.com>
2014-06-25 02:27:39 +0700

    Commit from egit

--- src/com/example/hello/HelloWorld.java
+++ src/com/example/hello/HelloWorld.java
@@ -3,10 +3,6 @@ package com.example.hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!");
-
-
-		System.out.println("This came from eclipse");
-		System.out.println("Test conflict!");
 		new PongGame();
 	}
 
