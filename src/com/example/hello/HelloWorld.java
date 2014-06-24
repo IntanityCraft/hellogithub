0d37bd9b5f2b31fddc253b47a9b0b79a8d508af6
godfavornoone <baebae_ac126@hotmail.com>
2014-06-25 00:07:39 +0700

    E dok

--- src/com/example/hello/HelloWorld.java
+++ src/com/example/hello/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!");
 		System.out.println("add a new line");
+		System.out.println("fuckyou");
 	}
 
 }
