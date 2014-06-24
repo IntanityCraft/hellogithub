79dc668303f798c92f801d7134eda3242c97b9fa
ArchSanta <santazaaa@gmail.com>
2014-06-25 00:36:10 +0700

    เพิ่ม jframe

--- src/com/example/hello/HelloWorld.java
+++ src/com/example/hello/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!");
 		System.out.println("Baezy Fat LOL++");
+		new PongGame();
 	}
 
 }
---
+++ src/com/example/hello/PongGame.java
@@ -0,0 +1,10 @@
+package com.example.hello;
+
+import javax.swing.JFrame;
+
+public class PongGame extends JFrame {
+	public PongGame() {
+		setSize(600, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
