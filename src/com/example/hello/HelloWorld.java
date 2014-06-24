ac24534e0196e66613438b777442799543ba8fe0
santazaaa <santazaaa@gmail.com>
2014-06-25 01:56:12 +0700

    Update HelloWorld.java

--- src/com/example/hello/HelloWorld.java
+++ src/com/example/hello/HelloWorld.java
@@ -5,22 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello Github!");
 
 
-
-		System.out.println("add a new line");
-		System.out.println("fuckyou");
-		System.out.println("Edok");
-
-		System.out.println("Baezy Fat LOL");
-
-
-		System.out.println("Baezy Fat++++");
-		System.out.println("Baezy Fat LOL");
-		
-		System.out.println("tum gu confuse e dok");
-
-
-
-		System.out.println("Baezy Fat LOL++");
+		System.out.println("Test conflict!");
 		new PongGame();
 	}
 
